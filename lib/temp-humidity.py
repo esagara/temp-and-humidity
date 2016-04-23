df35f5fa48c6bf80524d1945497627555e916518
Eric Sagara <esagara@Hal-2.local>
2016-04-22 23:41:17 -0700

    writing out to datafile

--- lib/temp-humidity.py
+++ lib/temp-humidity.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 import Adafruit_DHT
 
 data_file = "./data/readings.csv"
 
 sensor = Adafruit_DHT.DHT22
 pin = 4
+
+now = datetime.now().strftime("%Y-%m-%d %I:%M %p")
 
 def convert_temp(c):
     '''Converts temperature from Celsius `c` to Farenheit.'''
@@ -13,4 +16,8 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 temperature = convert_temp(temperature)
 
-print "The temperature is {0} degrees Farenheit and the humidity is {1} percent.".format(temperature, humidity)
+# print "The temperature is {0} degrees Farenheit and the humidity is {1} percent at {2}.".format(temperature, humidity, time)
+
+with open(data_file,'ab') as output:
+    record = ",".join([now, temperature, humidity])
+    output.write(record)
